6e9e97fa4b0cc40b584bc2677d4c455c09464638
Rasheed Bustamam <rasheed.bustamam@gmail.com>
2015-01-13 23:31:48 -0800

    Add gruntfile

---
+++ Gruntfile.js
@@ -0,0 +1,72 @@
+'use strict'
+
+var ngrok = require('ngrok');
+
+module.exports = function(grunt) {
+
+  // Load grunt tasks
+  require('load-grunt-tasks')(grunt);
+
+  require("matchdep").filterDev("grunt-*").forEach(grunt.loadNpmTasks);
+  // Grunt configuration
+  grunt.initConfig({
+    htmlhint: {
+      build: {
+        options: {
+            'tag-pair': true,
+            'tagname-lowercase': true,
+            'attr-lowercase': true,
+            'attr-value-double-quotes': true,
+            'doctype-first': true,
+            'spec-char-escape': true,
+            'id-unique': true,
+            'head-script-disabled': true,
+            'style-disabled': true
+        },
+        src: ['index.html', 'project-2048.html', 'project-mobile.html', 'project-webperf.html']
+      }
+    },
+    pagespeed: {
+      options: {
+        nokey: true,
+        locale: "en_GB",
+        threshold: 40
+      },
+      local: {
+        options: {
+          strategy: "desktop"
+        }
+      },
+      mobile: {
+        options: {
+          strategy: "mobile"
+        }
+      }
+    },
+    watch: {
+      html: {
+        files: ['index.html', 'project-2048.html', 'project-mobile.html', 'project-webperf.html'],
+        tasks: ['htmlhint']
+      }
+    }
+  });
+
+  // Register customer task for ngrok
+  grunt.registerTask('psi-ngrok', 'Run pagespeed with ngrok', function() {
+    var done = this.async();
+    var port = 8080;
+
+    ngrok.connect(port, function(err, url) {
+      if (err !== null) {
+        grunt.fail.fatal(err);
+        return done();
+      }
+      grunt.config.set('pagespeed.options.url', url);
+      grunt.task.run('pagespeed');
+      done();
+    });
+  });
+
+  // Register default tasks
+  grunt.registerTask('default', ['psi-ngrok']);
+}
